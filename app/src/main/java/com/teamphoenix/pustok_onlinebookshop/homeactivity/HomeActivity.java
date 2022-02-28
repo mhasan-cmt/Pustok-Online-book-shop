738d0ab0fc5dbe662c92cbea3480f14020105963
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-02-28 15:06:22 +0600

    Topbar, option menu, badge

--- app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/HomeActivity.java
+++ app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/HomeActivity.java
@@ -1,39 +1,34 @@
 package com.teamphoenix.pustok_onlinebookshop.homeactivity;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.NavHostController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.teamphoenix.pustok_onlinebookshop.R;
-import com.teamphoenix.pustok_onlinebookshop.homeactivity.fragments.BookShelfFragment;
-import com.teamphoenix.pustok_onlinebookshop.homeactivity.fragments.HomeFragment;
-import com.teamphoenix.pustok_onlinebookshop.homeactivity.fragments.ProfileFragment;
-
-import java.util.ArrayList;
+import com.teamphoenix.pustok_onlinebookshop.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
     private NavHostController navHostController;
     private BottomNavigationView bottomNavigationView;
-
+    ActivityHomeBinding homeBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ;
         setContentView(R.layout.activity_home);
+
 
         bottomNavigationView = findViewById(R.id.bottom_nav_view);
         NavController navController = Navigation.findNavController(HomeActivity.this, R.id.fragmentContainerView);
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
