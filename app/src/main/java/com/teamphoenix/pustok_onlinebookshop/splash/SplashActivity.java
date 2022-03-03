2c4fad2b2905307bbcade913fbc5e762b39f1ff9
rakhi336 <arohinur336@gmail.com>
2022-03-02 23:52:13 -0800

    splashActivity Added

---
+++ app/src/main/java/com/teamphoenix/pustok_onlinebookshop/splash/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.teamphoenix.pustok_onlinebookshop.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.teamphoenix.pustok_onlinebookshop.R;
+import com.teamphoenix.pustok_onlinebookshop.homeactivity.HomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
