3ef13f01dec004355b214440ec373b1a5469dda9
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-03-03 22:39:53 +0600

    splash screen upgraded

--- app/src/main/java/com/teamphoenix/pustok_onlinebookshop/splash/SplashActivity.java
+++ app/src/main/java/com/teamphoenix/pustok_onlinebookshop/splash/SplashActivity.java
@@ -5,22 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.teamphoenix.pustok_onlinebookshop.R;
 import com.teamphoenix.pustok_onlinebookshop.homeactivity.HomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private LottieAnimationView animationView;
+    private TextView appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        animationView = findViewById(R.id.animated_logo);
+        appName = findViewById(R.id.app_name);
+        animationView.animate().translationY(0).setDuration(2500).setStartDelay(0);
+        appName.animate().translationY(0).setDuration(2500).setStartDelay(0);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this, HomeActivity.class));
                 finish();
             }
-        },3000);
+        }, 3000);
+
     }
 }
