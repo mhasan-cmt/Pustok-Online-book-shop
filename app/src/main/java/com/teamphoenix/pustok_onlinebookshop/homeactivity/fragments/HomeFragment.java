02cea44fbb701bc6ba8654b14754ed8da07797a6
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-03-04 21:34:37 +0600

    cart activity added

--- app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/fragments/HomeFragment.java
+++ app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/fragments/HomeFragment.java
@@ -1,15 +1,26 @@
 package com.teamphoenix.pustok_onlinebookshop.homeactivity.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.teamphoenix.pustok_onlinebookshop.R;
+import com.teamphoenix.pustok_onlinebookshop.cart.CartActivity;
+import com.teamphoenix.pustok_onlinebookshop.homeactivity.HomeActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,52 +28,43 @@ import com.teamphoenix.pustok_onlinebookshop.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MaterialToolbar topActionBar;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
+    public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        topActionBar =view.findViewById(R.id.home_toolbar);
+        topActionBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId()==R.id.top_bar_cart_item){
+                    startActivity(new Intent(getActivity(), CartActivity.class));
+                } else if (item.getItemId() == R.id.top_bar_search_item) {
+                    Toast.makeText(getActivity(), "Search Clicked", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
+
 }
