e93412cc92cb35166685c4a4eadd39f8271a9b84
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-03-04 22:54:42 +0600

    exit dialog added

--- app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/HomeActivity.java
+++ app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.teamphoenix.pustok_onlinebookshop.homeactivity;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +24,8 @@ public class HomeActivity extends AppCompatActivity {
 
     private NavHostController navHostController;
     ActivityHomeBinding homeBinding;
+    private Dialog exitDialog;
+    private Button exit_btn_oky,exit_btn_no;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +37,33 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(HomeActivity.this, R.id.fragmentContainerView);
         NavigationUI.setupWithNavController(homeBinding.bottomNavView, navController);
 
+        //Exit Dialog
+        exitDialog = new Dialog(this);
+        exitDialog.setContentView(R.layout.custom_exit_dialog);
+        exitDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.custom_exit_dialog_bg));
+        exitDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        exitDialog.setCancelable(false);
+        exitDialog.setCanceledOnTouchOutside(false);
+        exitDialog.getWindow().getAttributes().windowAnimations = R.style.exit_dialog_animation;
+        exit_btn_no = exitDialog.findViewById(R.id.exit_btn_no);
+        exit_btn_oky = exitDialog.findViewById(R.id.exit_btn_oky);
+        exit_btn_oky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exitDialog.dismiss();
+                finish();
+            }
+        });
+        exit_btn_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exitDialog.dismiss();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        exitDialog.show();
     }
 }
