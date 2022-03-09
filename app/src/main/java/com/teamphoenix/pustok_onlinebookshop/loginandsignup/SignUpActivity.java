768ff491f49d6fee07bac5f6b4f190b3840fdfaa
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-03-09 11:15:23 +0600

    progressDialog and some variable naming convention fix

--- app/src/main/java/com/teamphoenix/pustok_onlinebookshop/loginandsignup/SignUpActivity.java
+++ app/src/main/java/com/teamphoenix/pustok_onlinebookshop/loginandsignup/SignUpActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.teamphoenix.pustok_onlinebookshop.R;
@@ -42,60 +41,68 @@ public class SignUpActivity extends AppCompatActivity implements onSignupListene
         }
     }
 
-    public int at2(){
-            if (etUserName.getText().toString().isEmpty()) {
-                Toast.makeText(this, "Enter name", Toast.LENGTH_SHORT).show();
-                etUserName.requestFocus();
-            } else if (etEmail.getText().toString().isEmpty()) {
-                Toast.makeText(this, "Enter an e-mail", Toast.LENGTH_SHORT).show();
-                etEmail.requestFocus();
-                return 0;
-            } else if (!Patterns.EMAIL_ADDRESS.matcher(etEmail.getText().toString()).matches()) {
-                Toast.makeText(this, "Enter a valid email!", Toast.LENGTH_SHORT).show();
-                etEmail.requestFocus();
-                return 0;
-            } else if (etPhone.getText().toString().isEmpty()) {
-                Toast.makeText(this, "Enter a Mobile Number!", Toast.LENGTH_SHORT);
-                etPhone.requestFocus();
-                return 0;
-            } else if (etPassword.getText().toString().isEmpty()) {
-                Toast.makeText(this, "Enter a password to continue!", Toast.LENGTH_SHORT).show();
-                etPassword.requestFocus();
-                return 0;
+    public int validateUserInput() {
+        if (etUserName.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Enter name", Toast.LENGTH_SHORT).show();
+            etUserName.requestFocus();
+        } else if (etEmail.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Enter an e-mail", Toast.LENGTH_SHORT).show();
+            etEmail.requestFocus();
+            return 0;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(etEmail.getText().toString()).matches()) {
+            Toast.makeText(this, "Enter a valid email!", Toast.LENGTH_SHORT).show();
+            etEmail.requestFocus();
+            return 0;
+        } else if (etPhone.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Enter a Mobile Number!", Toast.LENGTH_SHORT);
+            etPhone.requestFocus();
+            return 0;
+        } else if (etPassword.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Enter a password to continue!", Toast.LENGTH_SHORT).show();
+            etPassword.requestFocus();
+            return 0;
 
-            } else if (!etPassword.getText().toString().equals(etConfirmPassword.getText().toString())) {
-                Toast.makeText(this, "Password does not match!", Toast.LENGTH_SHORT).show();
-                etConfirmPassword.requestFocus();
-                return 0;
-            } else return 1;
+        } else if (!etPassword.getText().toString().equals(etConfirmPassword.getText().toString())) {
+            Toast.makeText(this, "Password does not match!", Toast.LENGTH_SHORT).show();
+            etConfirmPassword.requestFocus();
+            return 0;
+        } else return 1;
         return 0;
     }
-        public void signUp (View view){
-            if (at2() == 1) {
-                progressDialog = new ProgressDialog(this);
-                progressDialog.setTitle("Please Wait...!");
-                progressDialog.setMessage("Creating Account!");
-                progressDialog.show();
-                user = new User(etUserName.getText().toString(),
-                        etEmail.getText().toString(),
-                        etPhone.getText().toString(),
-                        etConfirmPassword.getText().toString());
-                firebaseAuthService.createUserWithEmailAndPassword(user, this);
-                progressDialog.dismiss();
-            }else{
-                Toast.makeText(this, "broken", Toast.LENGTH_SHORT).show();
-            }
-        }
 
-        @Override
-        public void onSuccess () {
-            Toast.makeText(this, "Account Created...!", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(this, HomeActivity.class));
-            finish();
-        }
-
-        @Override
-        public void onError (String msg){
-            Toast.makeText(this, "Account creation failed dut to some errors!", Toast.LENGTH_SHORT);
+    public void signUp(View view) {
+        if (validateUserInput() == 1) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Please Wait...!");
+            progressDialog.setMessage("Creating Account!");
+            progressDialog.setCancelable(false);
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+            firebaseAuthService.createUserWithEmailAndPassword(getDataFromUser(), this);
+        } else {
+            Toast.makeText(this, "broken", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private User getDataFromUser() {
+        user = new User(etUserName.getText().toString(),
+                etEmail.getText().toString(),
+                etPhone.getText().toString(),
+                etConfirmPassword.getText().toString());
+        return user;
+    }
+
+    @Override
+    public void onSuccess() {
+        progressDialog.dismiss();
+        Toast.makeText(this, "Account Created...!", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, HomeActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onError(String msg) {
+        progressDialog.dismiss();
+        Toast.makeText(this, "Account creation failed dut to some errors!", Toast.LENGTH_SHORT);
+    }
+}
