593ae20586074a0134a679b23d96d42d2906eb09
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-03-09 19:27:52 +0600

    login/signup bug fix

--- app/src/main/java/com/teamphoenix/pustok_onlinebookshop/service/FirebaseAuthService.java
+++ app/src/main/java/com/teamphoenix/pustok_onlinebookshop/service/FirebaseAuthService.java
@@ -56,6 +56,7 @@ public class FirebaseAuthService {
     }
 
     public boolean checkUserSignedIn() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         if (firebaseAuth.getCurrentUser() != null) {
             return true;
         } else {
