4dc6f4ad988038b8c985ecabd02992817ca405b1
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-03-13 17:26:48 +0600

    github conflicts fix

--- app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/tabs/CategoryTabFragment.java
+++ app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/tabs/CategoryTabFragment.java
@@ -1,24 +1,20 @@
 package com.teamphoenix.pustok_onlinebookshop.homeactivity.tabs;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 import com.teamphoenix.pustok_onlinebookshop.MODALs.CatagModal;
 import com.teamphoenix.pustok_onlinebookshop.R;
 import com.teamphoenix.pustok_onlinebookshop.firebase.Cateadapter;
@@ -31,7 +27,7 @@ public class CategoryTabFragment extends Fragment {
 
     RecyclerView recyclerView;
     Cateadapter cateadapter;
-    ArrayList<CatagModal>arrayList;
+    ArrayList<CatagModal> arrayList;
     FirebaseDatabase database;
     DatabaseReference databaseReference;
 
@@ -39,20 +35,18 @@ public class CategoryTabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-<<<<<<< HEAD:app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/FRAGMENTFORTABLAYOUT/CategoryTabFragment.java
-        View view= inflater.inflate(R.layout.fragment_contentfragment2, container, false);
-     recyclerView =view.findViewById(R.id.Catagorybooks);
-     recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-     database = FirebaseDatabase.getInstance();
-     databaseReference = database.getReference("Catagory");
+        View view = inflater.inflate(R.layout.fragment_category_tab, container, false);
+        recyclerView = view.findViewById(R.id.Catagorybooks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Catagory");
 
-     arrayList = new ArrayList<>();
-     cateadapter = new Cateadapter(arrayList, getContext());
-     recyclerView.setAdapter(cateadapter);
-
+        arrayList = new ArrayList<>();
+        cateadapter = new Cateadapter(arrayList, getContext());
+        recyclerView.setAdapter(cateadapter);
 
 
-return view;
+        return view;
     }
 
     @Override
@@ -61,12 +55,12 @@ return view;
             @Override
             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
                 arrayList.clear();
-                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
-                    CatagModal catagModal=dataSnapshot.getValue(CatagModal.class);
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    CatagModal catagModal = dataSnapshot.getValue(CatagModal.class);
                     arrayList.add(catagModal);
 
                 }
-cateadapter.notifyDataSetChanged();
+                cateadapter.notifyDataSetChanged();
             }
 
             @Override
@@ -76,12 +70,4 @@ cateadapter.notifyDataSetChanged();
         });
         super.onStart();
     }
-
-
-
-||||||| a2fb45a:app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/FRAGMENTFORTABLAYOUT/CategoryTabFragment.java
-        return inflater.inflate(R.layout.fragment_contentfragment2, container, false);
-=======
-        return inflater.inflate(R.layout.fragment_category_tab, container, false);
->>>>>>> writer:app/src/main/java/com/teamphoenix/pustok_onlinebookshop/homeactivity/tabs/CategoryTabFragment.java
-    }
+}
